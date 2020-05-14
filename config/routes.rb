29cd50fcacfc5de4f9766ae21844c77097816b09
sa-una-gi <k5.daha55@gmail.com>
2020-05-14 06:04:40 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   get '/contact', to:'static_pages#contact'
   
   get '/signup', to: 'users#new'
+  
+  post '/signup', to:'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
+  
+  resources :users
 end
