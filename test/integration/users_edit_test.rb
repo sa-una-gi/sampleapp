7b282a508bc4a75e3a68d018fc7368b28338fcf1
sa-una-gi <k5.daha55@gmail.com>
2020-06-14 15:49:54 +0000

    Finish user edit ,update,index,and destroy action

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,35 +20,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select "div.alert","The form contains 4 errors."
   end
-  
+
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_equal session[:forwarding_url], edit_user_url(@user)
     log_in_as(@user)
-    assert_nil session[:forwarding_url]
-    name = "Foo Bar"
+    assert_redirected_to edit_user_url(@user)
+    name  = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: {user: {name: name,
-                          email: email,
-                          password:   "",
-                          password_confirmation:""}}
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal name, @user.name
+    assert_equal name,  @user.name
     assert_equal email, @user.email
   end
   
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-  
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: {user: {name: @user.name,
-                          email: @user.email}}
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
+
 end
